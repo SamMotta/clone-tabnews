769fca0da6647c26640dfb3db64a81bacc1ad9fe
Samuel Motta <samuelsamuca953@gmail.com>
2024-11-18 09:51:54 -0300

    First page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <>
+            <h1>Elizabete, eu te amo! E se você me ama, manda áudio gritando que me ama!</h1>
+        </>
+    )
+}
+
+export default Home;
