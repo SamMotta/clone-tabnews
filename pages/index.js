a9d4f9dd79c6ba8ff4e3e3be6eff5fdd3476c90d
Samuel Motta <samuelsamuca953@gmail.com>
2024-11-22 16:02:13 -0300

    Formata arquivo `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 function Home() {
   return (
-    <>
-      <h1>
-        Elizabete, eu te amo! E se você me ama, manda áudio dizendo que me ama!
-      </h1>
-    </>
+    <h1>Elizabete, eu te amo! E se você me ama, manda áudio dizendo que me ama!</h1>
   );
 }
 
