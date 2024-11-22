e741f93b3997de8c599ac671d3e7ca6c2461d4ad
Samuel Motta <samuelsamuca953@gmail.com>
2024-11-22 12:20:17 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 function Home() {
-    return (
-        <>
-            <h1>Elizabete, eu te amo! E se você me ama, manda áudio dizendo que me ama!</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>
+        Elizabete, eu te amo! E se você me ama, manda áudio dizendo que me ama!
+      </h1>
+    </>
+  );
 }
 
 export default Home;
