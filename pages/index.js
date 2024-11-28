e9557d86665363239a3ae9bb32617aea00828c95
Samuel Motta <samuelsamuca953@gmail.com>
2024-11-28 16:36:33 -0300

    formata arquivos usando `npm run lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ function Home() {
   return (
     <>
       <h1>Em construção!</h1>
-      <h2>Muito em breve aqui se encontrará o shoptab, trazendo notícias sobre promoções e artigos relacionados a produtos.</h2>
+      <h2>
+        Muito em breve aqui se encontrará o shoptab, trazendo notícias sobre
+        promoções e artigos relacionados a produtos.
+      </h2>
     </>
   );
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,6 @@
-const URI = "http://localhost:3000";
+const URI = 'http://localhost:3000';
 
-test("GET to /api/v1/status should return 200", async () => {
+test('GET to /api/v1/status should return 200', async () => {
   const response = await fetch(`${URI}/api/v1/status`);
   expect(response.status).toBe(200);
 });
